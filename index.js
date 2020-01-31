7eeea4539630d6df4b889de6b65b4f2fe489f71a
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-01-31 19:58:00 -0300

    Creando el Controller de Autenticación

--- index.js
+++ index.js
@@ -14,7 +14,8 @@ app.use(express.json({ extended: true}));
 const PORT = process.env.PORT || 4000;
 
 // importar rutas
-app.use('/api/usuarios', require('./routes/usuarios'))
+app.use('/api/usuarios', require('./routes/usuarios'));
+app.use('/api/auth', require('./routes/auth'));
 
 
 // Arrancar la app
