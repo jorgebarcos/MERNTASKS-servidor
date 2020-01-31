91abe6fd5f298484c1ff4ac1693acc0e2f87c4d6
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-01-31 19:16:32 -0300

    Revisando si el usuario no ha sido registrado previamente

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,9 +1,18 @@
 const Usuario = require('../models/Usuario')
 
 exports.crearUsuario = async (req, res) => {
+
+    // extraer email y password 
+    const { email, password } = req.body;
     
     try {
-        let usuario;
+        // Revisar que el usuario registrado sea unico
+
+        let usuario = await Usuario.findOne({ email });
+
+        if(usuario) {
+            return res.status(400).json({ msg: 'El usuario ya existe'});
+        }
 
         // guardar el nuevo usuario
         usuario = new Usuario(req.body)
@@ -12,7 +21,7 @@ exports.crearUsuario = async (req, res) => {
         await usuario.save();
 
         // Mensaje de confirmación
-        res.send('Usuario creado correctamente')
+        res.json({ msg: 'Usuario creado correctamente'})
     } catch (error) {
         console.log(error);
         res.status(400).send('Hubo un error')
