3df334d8d3758532ece2285fb6c86ea886b5911e
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-01-31 21:23:22 -0300

    Verificando si el usuario esta autenticado para crear nuevos proyectos

--- controllers/proyectoController.js
+++ controllers/proyectoController.js
@@ -6,6 +6,11 @@ exports.creaProyecto = async (req, res) => {
     try {
         // Crear un nuevo proyecto
         const proyecto = new Proyecto(req.body);
+
+        // Guardar el creador via JWT
+        proyecto.creador = req.usuario.id;
+
+        // guardamos el proyecto
         proyecto.save();
         res.json(proyecto);
 
--- routes/proyectos.js
+++ routes/proyectos.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const proyectoController = require('../controllers/proyectoController')
+const auth = require('../middleware/auth')
 
 
 // Crea proyectos
 // api/proyectos
 router.post('/',
+    auth,
     proyectoController.creaProyecto
-)
+);
+
+router.get('/',
+    auth,
+    proyectoController.creaProyecto
+);
 
 module.exports = router;
