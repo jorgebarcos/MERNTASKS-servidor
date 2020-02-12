59db02f11e035e2382cd5564b26d9874ee762564
Jorge Alberto Barcos <jorgebarcos88@gmail.com>
2020-02-12 18:51:19 -0300

    Validar el Inicio de Sesión

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,27 +3,27 @@ const bcryptjs = require('bcryptjs');
 const { validationResult } = require('express-validator')
 const jwt = require('jsonwebtoken');
 
-exports.autenticarUsuario = async (req, res) => {
+exports.autenticarUsuario = async(req, res) => {
     // revisar si hay errores
     const errores = validationResult(req);
-    if( !errores.isEmpty() ) {
-        return res.status(400).json({errores: errores.array()})
+    if (!errores.isEmpty()) {
+        return res.status(400).json({ errores: errores.array() })
     }
 
     // extraer el email y password
-    const {email, password} = req.body;
+    const { email, password } = req.body;
 
     try {
         // Revisar que sea un usuario registrado
-        let usuario = await Usuario.findOne({email});
-        if(!usuario) {
-            return res.status(400).json({msg: 'El usuario no existe'})
+        let usuario = await Usuario.findOne({ email });
+        if (!usuario) {
+            return res.status(400).json({ msg: 'El usuario no existe' })
         }
 
         // Revisar el password
         const passCorrecto = await bcryptjs.compare(password, usuario.password);
-        if(!passCorrecto) {
-            return res.status(400).json({msg: 'Password Incorrecto'})
+        if (!passCorrecto) {
+            return res.status(400).json({ msg: 'Password Incorrecto' })
         }
 
         // Si todo es correcto crear y firmar el JWT
@@ -37,7 +37,7 @@ exports.autenticarUsuario = async (req, res) => {
         jwt.sign(payload, process.env.SECRETA, {
             expiresIn: 3600 // 1 hora
         }, (error, token) => {
-            if(error) throw error;
+            if (error) throw error;
 
             // Mensaje de confirmación
             res.json({ token });
@@ -50,13 +50,13 @@ exports.autenticarUsuario = async (req, res) => {
 }
 
 // obtiente que usuario esta autenticado
-exports.usuarioAutenticado = async (req, res) => {
+exports.usuarioAutenticado = async(req, res) => {
     try {
-        const usuario = await Usuario.findById(req.usuario.id);
-        res.json({usuario})
+        const usuario = await Usuario.findById(req.usuario.id).select('-password');
+        res.json({ usuario })
     } catch (error) {
         console.log(error);
-        res.status(500).json({msg: 'Hubo un error'});
-        
+        res.status(500).json({ msg: 'Hubo un error' });
+
     }
 }
--- routes/auth.js
+++ routes/auth.js
@@ -8,13 +8,7 @@ const auth = require('../middleware/auth');
 
 // Iniciar sesión
 // api/auth
-router.post('/', 
-[
-    
-    check('email', 'Agrega un email válido').isEmail(),
-    check('password', 'El password debe ser minimo de 6 caracteres').isLength({ min: 6})
-],
-authController.autenticarUsuario
+router.post('/', authController.autenticarUsuario
 
 );
 
